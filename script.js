312a3635acab9d4866aa68054a9d6fa55a511cd2
Studdnet <st.codding@gmail.com>
2022-11-24 00:45:14 +0300

    intermediate saving

---
+++ script.js
@@ -0,0 +1,8 @@
+const startNumber = 9;
+const flipCard = document.querySelector(".flip-card");
+
+[...flipCard.children].forEach(child => {
+	child.textContent = startNumber;
+});
+
+flipCard.classList.add("flip");
